298a3c7748094202c4e4d9cb0d180244dba7b148
tuyok242003 <hieudtph27992@gmail.com>
2024-03-27 08:50:56 +0700

    'update'

--- backend/server.js
+++ backend/server.js
@@ -1,52 +1,52 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable import/first */
-import path from "path"
-import express from "express"
-import dotenv from "dotenv"
-import cookieParser from "cookie-parser"
-dotenv.config()
-import connectDB from "./config/db.js"
-import productRoutes from "./routes/productRoutes.js"
-import userRoutes from "./routes/userRoutes.js"
-import orderRoutes from "./routes/orderRoutes.js"
-import uploadRoutes from "./routes/uploadRoutes.js"
-import contactRoutes from "./routes/contactRoutes.js"
-import postRoutes from "./routes/postRoutes.js"
-import categoryRoutes from "./routes/categoryRoutes.js"
-import voucherRoutes from "./routes/voucherRoutes.js"
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
-const port = process.env.PORT || 5000
+import path from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+dotenv.config();
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
+import contactRoutes from './routes/contactRoutes.js';
+import postRoutes from './routes/postRoutes.js';
+import categoryRoutes from './routes/categoryRoutes.js';
+import voucherRoutes from './routes/voucherRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+const port = process.env.PORT || 5000;
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.use("/api/products", productRoutes)
-app.use("/api/users", userRoutes)
-app.use("/api/orders", orderRoutes)
-app.use("/api/upload", uploadRoutes)
-app.use("/api/contacts", contactRoutes)
-app.use("/api/posts", postRoutes)
-app.use("/api/categories", categoryRoutes)
-app.use("/api/vouchers", voucherRoutes)
-app.get("/api/config/paypal", (req, res) => res.send({ clientId: process.env.PAYPAL_CLIENT_ID }))
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
+app.use('/api/contacts', contactRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/categories', categoryRoutes);
+app.use('/api/vouchers', voucherRoutes);
+app.get('/api/config/paypal', (req, res) => res.send({ clientId: process.env.PAYPAL_CLIENT_ID }));
 
-if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve()
-  app.use("/uploads", express.static("/var/data/uploads"))
-  app.use(express.static(path.join(__dirname, "/frontend/build")))
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static('/var/data/uploads'));
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")))
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
 } else {
-  const __dirname = path.resolve()
-  app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
-  app.get("/", (req, res) => {
-    res.send("API is running....")
-  })
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
 }
 // Route để lấy danh sách đơn hàng
 // app.get('/orders', async (req, res) => {
@@ -63,7 +63,7 @@ if (process.env.NODE_ENV === "production") {
 //   res.json(orders);
 // });
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`))
+app.listen(port, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`));
--- frontend/src/screens/Home/HomeScreen.tsx
+++ frontend/src/screens/Home/HomeScreen.tsx
@@ -2,14 +2,12 @@ import { ICategories } from '../../interfaces/OutShop';
 import { IProducts } from '../../interfaces/OutShop';
 import { useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 import Loader from '../../components/Loader';
 import Message from '../../components/Message';
 import Meta from '../../components/Meta';
 import PostCarousel from '../../components/PostCarousel.tsx';
 import Product from '../../components/Product';
 import { useGetProductsQuery } from '../../redux/query/apiSlice';
-import { HOME } from '../../constants/constants';
 const HomeScreen = () => {
   const { data, isLoading, error } = useGetProductsQuery();
   const [selectedCategory] = useState<ICategories | null>(null);
@@ -21,10 +19,6 @@ const HomeScreen = () => {
     <>
       <PostCarousel />
 
-      <Link to={HOME} className="btn btn-light mb-4">
-        Go Back
-      </Link>
-
       {isLoading ? (
         <Loader />
       ) : error ? (
@@ -33,17 +27,6 @@ const HomeScreen = () => {
         <>
           <Meta />
           <h1 hidden>{selectedCategory ? `Products in ${selectedCategory}` : 'Latest Products'}</h1>
-          {/* <div>
-            <Category onCategoryClick={handleCategoryClick} />
-            {selectedCategory && (
-              <button
-                className='btn btn-light mb-4'
-                onClick={() => setSelectedCategory(null)}
-              >
-                Show All
-              </button>
-            )}
-          </div> */}
           <Row>
             {data?.products.map((product: IProducts) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
